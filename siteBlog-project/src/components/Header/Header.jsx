8802aa6bcacf1945168e106db029655654ff56a1
Bruno Carlini <bruno.ccarlini@ultra.corp>
2023-11-08 21:00:04 -0300

    Adicionado função do Menu Burger

--- siteBlog-project/src/components/Header/Header.jsx
+++ siteBlog-project/src/components/Header/Header.jsx
@@ -1,22 +1,60 @@
-import { CiMenuBurger } from 'react-icons/ci'
+import { IoMenuOutline } from 'react-icons/io5'
 import styles from './header.module.scss'
+import './header.module.scss'
+import { useState } from 'react'
 
 export default function Header() {
+
+    const [activeMenu, setMenu] = useState(false)
+
+    let toggleMenu = () => {
+        setMenu(!activeMenu)
+    }
+
+
+
     return (
         <>
             <header className={styles.header}>
 
                 <div className={styles.container__header}>
                     <div>
-                        <CiMenuBurger size={45} />
-                    </div>
-
-                    <div>
                         LOGO
                     </div>
+
+                    <nav className={styles.container__menuDesktop}>
+                        <ul>
+                            <li>Início</li>
+                            <li>Sobre mim</li>
+                            <li>Contatos</li>
+                            <li>Psicoterapia</li>
+                            <li>Depoimentos</li>
+                            <li>Blog</li>
+                            <li>Serviços</li>
+                        </ul>
+                    </nav>
+
+                    <div className={styles.container__menuBurger} onClick={() => toggleMenu()}>
+                        <IoMenuOutline size={45} />
+                    </div>
+
                 </div>
 
             </header>
+
+            <nav className={activeMenu ? `${styles.menu} ${styles.menu__active}` : `${styles.menu} ${styles.menu__disabled}`}>
+                <ul >
+                    <li>Início</li>
+                    <li>Sobre mim</li>
+                    <li>Contatos</li>
+                    <li>Psicoterapia</li>
+                    <li>Depoimentos</li>
+                    <li>Blog</li>
+                    <li>Serviços</li>
+                </ul>
+            </nav>
+
+            <h1>Teste</h1>
         </>
     )
 }
