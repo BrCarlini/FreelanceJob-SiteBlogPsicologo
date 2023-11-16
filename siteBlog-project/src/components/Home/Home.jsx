62a57a100186b5f1d62210b91ae212b43b39a8c8
Bruno Carlini <bruno.ccarlini@ultra.corp>
2023-11-16 14:26:29 -0300

    Estrutura dos componentes home e button finalizados

--- siteBlog-project/src/components/Home/Home.jsx
+++ siteBlog-project/src/components/Home/Home.jsx
@@ -1,9 +1,12 @@
+import Button from '../Button/Button'
 import styles from './home.module.scss'
 
 export default function Home() {
     return (
         <>
             <section className={styles.container__sectionHome}>
+
+                
 
                 <div className={styles.container__textHome}>
                     <h1>Saúde Emocional</h1>
@@ -25,13 +28,13 @@ export default function Home() {
                         reiciendis est consectetur distinctio magnam eveniet omnis,
                         quod doloribus beatae iste aliquid id exercitationem?
                     </p>
+
+                    <Button text="Tire suas dúvidas"/>
                 </div>
 
                 <div className={styles.container__imageHome}>
-                    <img src="https://images.pexels.com/photos/4098374/pexels-photo-4098374.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                    
                 </div>
-
-
             </section>
         </>
     )
